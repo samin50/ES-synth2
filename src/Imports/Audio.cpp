1c0408d3a305f460118875b7bc01697944c274a2
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-03-19 00:46:36 +0000

    Update Audio.cpp

--- src/Imports/Audio.cpp
+++ src/Imports/Audio.cpp
@@ -1,9 +1,12 @@
 #include "ourLibrary.h"
+#include <iostream>
 
 //Function for audio generation
+
 void sampleISR() {
     static uint32_t phaseAcc[POLYPHONY];
     uint32_t Vout = 0;
+    bool choose = true;
     int8_t octaveOffset;
     float volumeMod = VOLUMEMOD/10;
     //If not master, do not play
@@ -28,27 +31,37 @@ void sampleISR() {
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-            if ((phaseAcc[i] >> 24) < 7) {
+            if ((phaseAcc[i] >> 24) < 32) {
                 Vout -= 16;
             } else {
                 Vout += 16;
             }
-        } if(WAVETYPE == 2) { //Sine wave
+        } else if(WAVETYPE == 2) { //Sine wave
             if(octaveOffset > 0) {
                 phaseAcc[i] += (currentStepSize[i] >> octaveOffset);
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-            Vout += (int)(sin((float)(phaseAcc[i] >> 24)*2*3.1415926/(float)13)*13/POLYPHONY);
+            Vout += 0;
+            //Vout += (int)(sin((float)(phaseAcc[i] >> 24)*2*3.1415926/(float)13)*13/POLYPHONY);
         } else if(WAVETYPE == 3) { //Triangular wave
             if(octaveOffset > 0) {
                 phaseAcc[i] += (currentStepSize[i] >> octaveOffset);
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-            Vout += (int)(((phaseAcc[i]>>24)%13)*2.0/13 - 1.0) * 13/POLYPHONY;
-        }
 
+            if ((phaseAcc[i] >> 24) > 128)
+                choose = false;
+            else if ((phaseAcc[i] >> 24) < 64)
+                choose = true;
+
+            if (choose) {
+                Vout += floor(((phaseAcc[i] >> 24) - 128)/POLYPHONY);
+            } else {
+                Vout -= floor(((phaseAcc[i] >> 24) - 128)/POLYPHONY);
+            }
+        }
     }
     //Volume control here
     analogWrite(OUTR_PIN, (((Vout + 128)*VOLUMEMOD) >> 3));
