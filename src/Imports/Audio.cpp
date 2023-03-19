8d15ec43c2f59ad8b245fec02c6fb0de016c1f14
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-03-19 00:48:02 +0000

    change back audio.cpp

--- src/Imports/Audio.cpp
+++ src/Imports/Audio.cpp
@@ -1,12 +1,9 @@
 #include "ourLibrary.h"
-#include <iostream>
 
 //Function for audio generation
-
 void sampleISR() {
     static uint32_t phaseAcc[POLYPHONY];
     uint32_t Vout = 0;
-    bool choose = true;
     int8_t octaveOffset;
     float volumeMod = VOLUMEMOD/10;
     //If not master, do not play
@@ -31,7 +28,7 @@ void sampleISR() {
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-            if ((phaseAcc[i] >> 24) < 32) {
+            if ((phaseAcc[i] >> 24) < 7) {
                 Vout -= 16;
             } else {
                 Vout += 16;
@@ -42,26 +39,20 @@ void sampleISR() {
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-            Vout += 0;
+            //FIX THIS - OPTIMISE
             //Vout += (int)(sin((float)(phaseAcc[i] >> 24)*2*3.1415926/(float)13)*13/POLYPHONY);
+            Vout += 0;
         } else if(WAVETYPE == 3) { //Triangular wave
             if(octaveOffset > 0) {
                 phaseAcc[i] += (currentStepSize[i] >> octaveOffset);
             } else {
                 phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
             }
-
-            if ((phaseAcc[i] >> 24) > 128)
-                choose = false;
-            else if ((phaseAcc[i] >> 24) < 64)
-                choose = true;
-
-            if (choose) {
-                Vout += floor(((phaseAcc[i] >> 24) - 128)/POLYPHONY);
-            } else {
-                Vout -= floor(((phaseAcc[i] >> 24) - 128)/POLYPHONY);
-            }
+            //FIX THIS - OPTIMISE
+            //Vout += (int)(((phaseAcc[i]>>24)%13)*2.0/13 - 1.0) * 13/POLYPHONY;
+            Vout += 0;
         }
+
     }
     //Volume control here
     analogWrite(OUTR_PIN, (((Vout + 128)*VOLUMEMOD) >> 3));
