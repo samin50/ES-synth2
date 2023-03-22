5cb112d743acee303c48dd1e788fc8331df36887
Shaanukstar123 <shaanukag0@gmail.com>
2023-03-22 23:11:53 +0000

    All audio fixed

--- src/Imports/Audio.cpp
+++ src/Imports/Audio.cpp
@@ -12,16 +12,13 @@ void sampleISR() {
     }
     //Polyphonic keypresses
     for(int i = 0; i < POLYPHONY; i++) {
-        // if (accumulatorMap[i] == NULL) {
-        //     continue;
-        // }
-         if (octaveOffset > 0) {
+        //Obtain octave information from accumulatorMap
+        octaveOffset = 4-accumulatorMap[i];
+        if (octaveOffset > 0) {
             phaseAcc[i] += (currentStepSize[i] >> octaveOffset);
         } else {
             phaseAcc[i] += (currentStepSize[i] << -octaveOffset);
         }
-        //Obtain octave information from accumulatorMap
-        octaveOffset = 4-accumulatorMap[i];
         //If the wave is sawtooth - simple addition
         if(WAVETYPE == 0) {
             Vout += ((phaseAcc[i] >> 24) - 128)/POLYPHONY;
